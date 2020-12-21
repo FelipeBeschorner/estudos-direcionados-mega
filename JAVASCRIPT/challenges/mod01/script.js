2c5ebcb38791a6c519d3d3eb275c4059a43aca8f
Felipe Beschorner <felipebeschorner@hotmail.com>
2020-12-21 16:58:30 -0300

    challenges

---
+++ JAVASCRIPT/challenges/mod01/script.js
@@ -0,0 +1,31 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun); 
