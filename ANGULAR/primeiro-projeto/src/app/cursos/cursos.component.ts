dfe792073a62f7a977dbedf04fd6210d0f0509a1
Felipe Beschorner <felipebeschorner@hotmail.com>
2021-01-07 11:14:54 -0300

    Angular start

---
+++ ANGULAR/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css'],
+})
+export class CursosComponent implements OnInit {
+  nomePortal: String;
+  cursos: String[];
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'https://loiane.training';
+    this.cursos = this.cursosService.getCursos();
+  }
+
+  ngOnInit(): void {}
+}
